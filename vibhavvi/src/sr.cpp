2428d85da4f551f7cc056527bd97f171b714cc58
Vibhav Yawalkar <vibhavyawalkar@gmail.com>
2020-04-02 21:16:16 -0400

    Selective Repeat working partially, need to fix a bug

--- vibhavvi/src/sr.cpp
+++ vibhavvi/src/sr.cpp
@@ -23,13 +23,13 @@ using namespace std;
 int send_base = 0;
 int nextseqnum = 0;
 int N_sender;
-float t = 12.00;
+float t = 20.00;
 struct senderMsg {
 	char message[20];
 	float start_time;
 	int acked;
 };
-struct senderMsg senderBuffer[1001] = {{0}};
+struct senderMsg senderBuffer[1001];
 vector<struct msg> buffer;
 
 /* Receiver Data */
@@ -41,24 +41,24 @@ struct receiverMsg {
 	int acked;
 };
 
-struct receiverMsg receiverBuffer[1010] = {{0}};
+struct receiverMsg receiverBuffer[1010];
 
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(struct msg message)
 {
-	cout << "A_output called" << endl;
+	cout << "A_output called, send_base: "<< send_base << " nextseqnum :" << nextseqnum << " N_sender: " << N_sender << endl;
 	buffer.push_back(message);
 	if(nextseqnum < send_base + N_sender) {
 		struct pkt mypkt;
 		memset(&mypkt, 0, sizeof(mypkt));
 		mypkt.seqnum = nextseqnum;
-		mypkt.acknum = 0;
+		mypkt.acknum = 0; // Setting 2 when the ACK is not received and 1 when the ack is received
 		int checksum = mypkt.acknum + mypkt.seqnum;	
 		for(int i = 0; i < 20; i++) {
 			mypkt.payload[i] = buffer[nextseqnum].data[i];
-			senderBuffer[nextseqnum].message[i] = mypkt.payload[i];
+			senderBuffer[nextseqnum].message[i] = buffer[nextseqnum].data[i];
 			checksum += mypkt.payload[i];
 		}
 		mypkt.checksum = checksum;
@@ -66,12 +66,12 @@ void A_output(struct msg message)
 		for(int i = 0; i < 20; i++)
 			cout << mypkt.payload[i];
 		cout << " checksum:" << mypkt.checksum;
-		senderBuffer[nextseqnum].acked = 0;
+		senderBuffer[nextseqnum].acked = 2;
 		senderBuffer[nextseqnum].start_time = get_sim_time(); /* time when the packet was sent */ 
 		cout << " send time: " << get_sim_time() << endl;
 		tolayer3(0, mypkt);
-		if(send_base == nextseqnum)
-			starttimer(0, 2.00);
+		//if(send_base == nextseqnum)
+		//	starttimer(0, 1.00);
 		nextseqnum++;
 	}
 }
@@ -82,52 +82,64 @@ void A_input(struct pkt packet)
 	cout << "A_input called" << endl;
 	int checksum = packet.acknum + packet.seqnum;
 	if(checksum == packet.checksum) {
-		cout << "ACK not corrupt" << endl;
-		if(packet.acknum < send_base + N_sender) {
+		cout << "ACK not corrupt, received for seqnum:" << packet.acknum << endl;
+		if(packet.acknum < send_base + nextseqnum/*N_sender*/) {
 		/* Mark the packet as received/acked, if it lies in the window */
 			senderBuffer[packet.acknum].acked = 1;
 		}
 		if(packet.acknum == send_base) {
+
 		/* The window base if moved forward to the unacknowledged packet with the smallest sequence number*/
-			for(int i = send_base; i < N_sender; i++)
-				if(senderBuffer[i].acked == 1)
-					send_base++; 
+			for(int i = send_base; i < nextseqnum/*send_base + N_sender*/; i++)
+			{
+				if(senderBuffer[i].acked == 1) {
+					send_base++;
+					cout << "Incremented send_base : " << send_base << endl;
+				}
+			}
 		}
+		
 	}
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
-	cout << "A_timerinterrupt called" << endl;
-
-	for(int i = send_base; i < N_sender; i++)
+	cout << "A_timerinterrupt called at " << get_sim_time() << endl;
+	cout << "Value of send base " << send_base << endl;
+	for(int i = send_base; i < nextseqnum; i++)
 	{
 		float diff = get_sim_time() - senderBuffer[i].start_time;
-		if(senderBuffer[i].acked == 0 && abs((int)diff) >= (int)t)
+		//cout << "Sendbuffer start time: " << senderBuffer[i].start_time << endl;
+		//cout << "diff: " << abs((int)diff) << " " << t << endl;
+		if(senderBuffer[i].acked == 2 && diff >= (int)(t))
 		{
+			cout << "diff: " << diff << endl;
 			/* Resend the packets who's ack is lost or not received by the receiver */
 			struct pkt mypkt;
                 	memset(&mypkt, 0, sizeof(mypkt));
                 	mypkt.seqnum = i;
                 	mypkt.acknum = 0;
+			cout << "Send Buffer contains ";
                 	int checksum = mypkt.acknum + mypkt.seqnum;
                 	for(int j = 0; j < 20; j++) {
                         	mypkt.payload[j] = senderBuffer[i].message[j];
+				cout << senderBuffer[i].message[j];
                         	checksum += mypkt.payload[j];
                 	}
+			cout << endl;
                 	mypkt.checksum = checksum;
                 	cout << "Sending pkt seqno: " << mypkt.seqnum << "msg : ";
                 	for(int j = 0; j < 20; j++)
                         	cout << mypkt.payload[j];
                 	cout << " checksum:" << mypkt.checksum << endl;
-                	senderBuffer[i].acked = 0;
+                	senderBuffer[i].acked = 2;
                 	senderBuffer[i].start_time = get_sim_time(); /* time when the packet was sent */
                 	tolayer3(0, mypkt); 
 		}
 	}
 	//stoptimer(0);
-	starttimer(0, 2.00);
+	starttimer(0, 1.00);
 }  
 
 /* the following routine will be called once (only) before any other */
@@ -138,7 +150,7 @@ void A_init()
 	send_base = 0;
 	nextseqnum = 0;
 	N_sender = getwinsize();
-//	starttimer(0, t);
+	starttimer(0, 1.00);
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
@@ -152,8 +164,12 @@ void B_input(struct pkt packet)
 		checksum += packet.payload[i];
 
 	if(packet.checksum == checksum) {
-        	for(int j = 0; j < 20; j++)
+		cout << "Packet received ";
+        	for(int j = 0; j < 20; j++) {
                 	receiverBuffer[packet.seqnum].message[j] = packet.payload[j];
+			cout << packet.payload[j];
+		}
+		cout << endl;
 
 		if(packet.seqnum >= rcv_base && packet.seqnum < rcv_base + N_receiver) {
 			struct pkt myackpkt;
@@ -166,7 +182,7 @@ void B_input(struct pkt packet)
 			receiverBuffer[packet.seqnum].acked = 1;
 			if(packet.seqnum == rcv_base)
 			{
-				for(int k = rcv_base; k < N_receiver; k++)
+				for(int k = rcv_base; k < rcv_base + N_receiver; k++)
 				{
 					if(receiverBuffer[k].acked == 1)
 					{
@@ -196,5 +212,5 @@ void B_init()
 	cout << "B_init called" << endl;
 	rcv_base = 0;
 	N_receiver = getwinsize();
-	memset(receiverBuffer, 0, sizeof(receiverBuffer));
+	//memset(receiverBuffer, 0, sizeof(receiverBuffer));
 }
